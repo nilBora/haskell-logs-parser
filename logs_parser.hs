21dabd043d24307c1b2a50efd8202237ac819918
Nil Borodulia <nil.b@oxtech.org>
2023-10-10 15:15:08 +0300

    first commit

---
+++ logs_parser.hs
@@ -0,0 +1,19 @@
+import Data.List
+import Data.String
+
+main = do
+    log <- readFile "log.txt"
+    let logLines = lines log
+    let logLinesWithIndex = zip [0..] logLines
+    let logLinesWithIndexFiltered = filter (\(i, line) -> "WARNING" `isInfixOf` line) logLinesWithIndex
+    let logLinesWithIndexFilteredMapped = map (\(i, line) -> (i, (words line) !! 1)) logLinesWithIndexFiltered
+    let logLinesWithIndexFilteredMappedSorted = sortOn snd logLinesWithIndexFilteredMapped
+    let logLinesWithIndexFilteredMappedSortedMapped = map (\(i, date) -> (logLines !! i)) logLinesWithIndexFilteredMappedSorted
+    let logLinesWithIndexFilteredMappedSortedMappedFiltered = filter (\line -> "ERROR" `isInfixOf` line) logLinesWithIndexFilteredMappedSortedMapped
+    let logLinesWithIndexFilteredMappedSortedMappedFilteredMapped = map (\line -> (words line) !! 1) logLinesWithIndexFilteredMappedSortedMappedFiltered
+    let logLinesWithIndexFilteredMappedSortedMappedFilteredMappedFiltered = filter (\date -> "2018-06-17" `isInfixOf` date) logLinesWithIndexFilteredMappedSortedMappedFilteredMapped
+    let logLinesWithIndexFilteredMappedSortedMappedFilteredMappedFilteredMapped = map (\date -> (words date) !! 1) logLinesWithIndexFilteredMappedSortedMappedFilteredMappedFiltered
+
+    -- Println each element logLinesWithIndexFiltered
+    mapM_ print logLinesWithIndexFiltered
+    --print logLinesWithIndexFiltered
